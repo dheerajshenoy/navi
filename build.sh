52668b58e5074fe1761ec443be5bc4beca140710
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-02-16 04:14:56 +0530

    fixed git clone

--- build.sh
+++ build.sh
@@ -1,38 +1,12 @@
 #!/bin/sh
 
 root=$(pwd)
-
-PARSED=$(getopt -o a:f --long argument:,flag -- "$@") || exit 1
-eval set -- "$PARSED"
-
-show_help() {
-    echo "Usage: $0 [OPTIONS]"
-    echo "Options:"
-    echo "  --clean            Cleans installation"
-    echo "  --help             Show this help message"
-    exit 0
-}
-
-while true; do
-    case "$1" in
-        -c|--clean) shift 2 ;;
-        -h|--help) shift ;;
-        --) shift; break ;;
-        *) break ;;
-    esac
-done
-
-if [[ ! -d "external" ]]; then
-    mkdir external
+mkdir -p external
+if [[ ! -d "external/KDDockWidgets" ]]; then
+    git clone --depth=1 https://github.com/KDAB/KDDockWidgets external/KDDockWidgets
 fi
-
-git clone --depth=1 https://github.com/KDAB/KDDockWidgets external/KDDockWidgets
 cd external/KDDockWidgets
-
-if [[ ! -d "build" ]]; then
-    mkdir build
-fi
-
+mkdir -p build
 cd build
 cmake .. -G Ninja -DKDDockWidgets_QT6=ON \
     -DKDDockWidgets_EXAMPLES=false \
@@ -40,15 +14,10 @@ cmake .. -G Ninja -DKDDockWidgets_QT6=ON \
     -DKDDockWidgets_FRONTENDS="qtwidgets" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX=/usr
-
 cmake --build .
 sudo cmake --build . --target install
 cd $root
-
-if [[ ! -d "build" ]]; then
-    mkdir build
-fi
-
+mkdir -p build
 cd build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE="Release"
 sudo ninja install
