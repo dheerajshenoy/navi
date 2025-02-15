48036001a6727406a2cab4c46acd83a83c95edc6
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-02-15 18:12:34 +0530

    remove binary

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 echo "Uninstalling navifm and it's libraries."
 echo "The config folder for navifm (~/.config/navifm/) is not deleted"
 echo "if you have created it"
+sudo rm -rf /usr/bin/navifm
 sudo rm -rf /usr/share/navifm
 sudo rm -rf /usr/include/kddockwidgets-qt6/
 sudo rm -rf /usr/share/applications/navifm.desktop
